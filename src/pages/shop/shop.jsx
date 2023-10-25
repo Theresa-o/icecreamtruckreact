a12de87d1087101deff4de32583eef3f46c3a00b
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-10-24 21:56:38 -0700

    Implement route access to product detail

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -36,18 +36,38 @@ const Shop = () => {
   }, []);
   return (
     <div>
-      <div className="shop">
+      <div className="container mx-auto py-8">
         <div className="shopTitle">
-          <h1>IceCreamIe</h1>
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+            Our Products
+          </h1>
         </div>
         <div className="iceCreamItems">
           <ul>
             {foodItems.map((foodItem) => (
               <li key={foodItem.id}>
                 <Link to={`/fooditem/${foodItem.id}`}>
-                  <img src={foodItem.image} alt={foodItem.name} />
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                      <img
+                        src={foodItem.image}
+                        alt={foodItem.name}
+                        className="w-full h-48 object-cover mx-auto max-w-xs"
+                      />
+                      <div className="p-4">
+                        <h2 className="text-xl font-semibold text-gray-800">
+                          {foodItem.name}
+                        </h2>
+                        <p className="text-indigo-700 font-bold mt-2">
+                          ${foodItem.price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* <img src={foodItem.image} alt={foodItem.name} />
                   <strong>{foodItem.name}</strong>
-                  <p>Price: ${foodItem.price}</p>
+                  <p>Price: ${foodItem.price}</p> */}
                 </Link>
               </li>
             ))}
